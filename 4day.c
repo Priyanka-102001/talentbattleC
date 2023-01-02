25e1c0ce6ac7a076f4b99d0ca3cf0f5b115fb532
Priyanka-102001 <87640810+Priyanka-102001@users.noreply.github.com>
2023-01-02 20:53:33 +0530

    Create 4day.c

---
+++ 4day.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("enter the number");
+ scanf("%d",&n);
+ 
+ if(n>0)
+ {
+ printf("number is positive");
+ }
+ else if(n==0)
+ {
+ printf("number is neither positive nor negative");
+ 
+ }
+ else
+ {
+ printf("number is negative");
+ }
+ return 0;
+}
